5ad9944564e53e0161cb93715048f89f221f048e
jose pinto <josedfpinto@gmail.com>
2022-11-29 15:16:15 +0000

    show de user page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :streets, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :streets
+  resources :users, only: [:show]
 end
