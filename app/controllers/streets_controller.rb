e6b19528aa2babc309fe1b7840a4208969e0e0fc
jose pinto <josedfpinto@gmail.com>
2022-12-02 12:56:42 +0000

    counter working

--- app/controllers/streets_controller.rb
+++ app/controllers/streets_controller.rb
@@ -9,12 +9,14 @@ class StreetsController < ApplicationController
   def index
     @streets = Street.all
     # The `geocoded` scope filters only flats with coordinates
+    @counter = Counter.new
     @markers = @streets.geocoded.map do |street|
       {
         lat: street.latitude,
         lng: street.longitude,
         info_window: render_to_string(partial: "info_window", locals: { street: street }),
-        image_url: helpers.asset_url("logowhite.png")
+        image_url: helpers.asset_url("logowhite.png"),
+        counter_btn: render_to_string(partial: "shared/counter_btn", locals: { street: street, counter: @counter })
       }
     end
     # @streetbike = Street.all
