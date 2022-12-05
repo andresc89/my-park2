b794d6f527b938574ffdc724dadf2e20668cbb46
andre custodio <andre.sc@outlook.pt>
2022-12-05 15:26:03 +0000

    parking counter working, counts all parks separately

--- app/controllers/streets_controller.rb
+++ app/controllers/streets_controller.rb
@@ -8,7 +8,6 @@ class StreetsController < ApplicationController
 
   def index
     @streets = Street.all
-    # The `geocoded` scope filters only flats with coordinates
     @counter = Counter.new
     @markers = @streets.geocoded.map do |street|
       {
@@ -17,17 +16,38 @@ class StreetsController < ApplicationController
         info_window: render_to_string(partial: "info_window", locals: { street: street }),
         image_url: helpers.asset_url("logowhite.png"),
         counter_btn: render_to_string(partial: "shared/counter_btn", locals: { street: street, counter: @counter })
-
       }
     end
-    # @streetbike = Street.all
-    # @streetbike = @streetbike.geocoded.map do |street|
-    #   {
-    #     lat: street.latitude,
-    #     lng: street.longitude,
-    #     info_window: render_to_string(partial: "info_window", locals: { street: street }),
-    #     image_url: helpers.asset_url("logobike.png")
-    #   }
-    # end
+  end
+
+  # def update
+  #   @street = Street.find(params[:id])
+  #   @counter = Counter.new
+  #   @street.availability = 50
+  #   if @counter.save
+  #     @street.availability += 5
+  #   end
+  # end
+
+
+  private
+
+  def street_params
+    params.require(:street).permit([:address, :availability])
   end
 end
+  # def park
+  #   @counter = Counter.new
+  #   @street = Street.find(params[:street_id])
+  #   @street.availability += 10
+  #   @counter.save
+  # end
+  # @streetbike = Street.all
+  # @streetbike = @streetbike.geocoded.map do |street|
+  #   {
+  #     lat: street.latitude,
+  #     lng: street.longitude,
+  #     info_window: render_to_string(partial: "info_window", locals: { street: street }),
+  #     image_url: helpers.asset_url("logobike.png")
+  #   }
+  # end
