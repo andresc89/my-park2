ba83bfe6082aef697543c3acb404e3105f2d3c44
David Deus <davidafonsodeus@outlook.com>
2022-12-08 14:12:15 +0000

    dependency destroy

--- app/models/street.rb
+++ app/models/street.rb
@@ -1,5 +1,5 @@
 class Street < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  has_many :counters
+  has_many :counters, dependent: :destroy
 end
